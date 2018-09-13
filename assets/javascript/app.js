48ecb4d9006e17d6da678d059ad7586210ad12e0
jean.chung <jeannchung@gmail.com>
2018-09-13 12:51:02 -0700

    updating

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,22 +3,57 @@ $(document).ready(function () {
     var counter = setInterval(timer, 1000);
 
     function timer() {
-        count = count - 1;
-        
-        if (count <= 0) {
+        correctAnswers = 0
+
+        if (count > 0) {
+            count = count - 1;
+            console.log(count)
+        } else if (count === 0) {
             clearInterval(counter);
             $('#content').empty()
             $('#content').append(`<div class="row text-black justify-content-center m-1">
             <h2 id="timeRemaining"></h2>
-            </div>`);    
-            $('#timeRemaining').text('All done! Checking your answers...')
-            return;
+            </div>`);
+            $('#timeRemaining').html('All done! Checking your answers...')
         } else if (count === 15) {
             $('#timeRemaining2').text("15 seconds left!")
         } else if (count === 10) {
             $('#timeRemaining2').html(`<br>`)
+        } else if (count < 0) {
+            if ($("#correct1").attr("checked", true)) {
+                correctAnswers++
+            } else if ($("#correct2").attr("checked", true)) {
+                correctAnswers++
+            } else if ($("#correct3").attr("checked", true)) {
+                correctAnswers++
+            } else if ($("#correct4").attr("checked", true)) {
+                correctAnswers++
+            } else if ($("#correct5").attr("checked", true)) {
+                correctAnswers++
+            }
+            return
+            $('#timeRemaining2').html(`You got ${correctAnswers} out of 5 questions correct.`)
         }
+
+
         $('#timeRemaining').text(`Time Remaining: ${count}`)
     }
+
+
+
+    // if (count <= 0) {
+    //     clearInterval(counter);
+    //     $('#content').empty()
+    //     $('#content').append(`<div class="row text-black justify-content-center m-1">
+    //     <h2 id="timeRemaining"></h2>
+    //     </div>`);    
+    //     $('#timeRemaining').text('All done! Checking your answers...')
+    //     return;
+    // } else if (count === 15) {
+    //     $('#timeRemaining2').text("15 seconds left!")
+    // } else if (count === 10) {
+    //     $('#timeRemaining2').html(`<br>`)
+    // }
+    // $('#timeRemaining').text(`Time Remaining: ${count}`)
 
 })
